e0eafdf8b939614ed018c93e65864b35fc8203cb
ZhannaMelnyk71 <11mazourenko@gmail.com>
2023-02-26 17:32:49 +0200

    task-2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEL = ingredients.map(ingredient => {
+  const ingredientsNameEl = document.createElement("li")
+ingredientsNameEl.textContent = ingredient;
+// console.log(ingredientsNameEl.textContent)
+  ingredientsNameEl.classList.add("item");
+  
+  return ingredientsNameEl;
+})
+
+
+const ingredientsListEl = document.querySelector("#ingredients")
+ingredientsListEl.after(...ingredientsEL)
