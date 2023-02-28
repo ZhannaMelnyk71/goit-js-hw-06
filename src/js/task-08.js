8c2f92a7f69ebd70785a916cad3e8cb7a7a1eda5
ZhannaMelnyk71 <11mazourenko@gmail.com>
2023-02-28 23:18:22 +0200

    task-8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,27 @@
 
+const formEl = document.querySelector('.login-form')
+
+
+const handlerFormSubmit = (event) => {
+    event.preventDefault();
+
+    const email = event.currentTarget.elements.email.value
+    console.log(email)
+    const password = event.currentTarget.elements.password.value
+    console.log(password)
+
+    const formData = { email, password }
+    console.log(formData)
+
+    if (email=== "" || password === "") {
+        return alert('Заповніть всі поля')
+    }
+
+    console.log(`Email: ${email}, Password: ${password}`);
+    
+    event.currentTarget.reset();
+
+
+}
+
+formEl.addEventListener('submit', handlerFormSubmit);
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnEl = document.querySelector('.change-color')
+console.log(btnEl)
+
+const bgColorEl = document.querySelector('.color')
+console.log(bgColorEl)
+
+const handleChangeColor = (event) => {
+  
+}
+
+btnEl.addEventListener("click", handleChangeColor)
